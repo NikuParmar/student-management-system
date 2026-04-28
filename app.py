1c556c5882835d73d45d5fec1ae9aa65984e6207
Nikita parmar <parmarnikita9898@gmail.com>
2026-04-28 14:11:58 +0530

    Fix Python comment syntax

--- app.py
+++ app.py
@@ -194,6 +194,6 @@ if __name__ == '__main__':
     print("  Password: admin123")
     print("=" * 50)
     
-    //app.run(debug=True)
+    #app.run(debug=True)
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port, debug=False)
